9257c0c67966715f2b8287334d7f531aadec77b3
Marko Pranjic <marko.pranjic@ijs.si>
2022-04-21 13:15:05 +0200

    Rewrite the file in a more idiomatic style

--- extract_collocations.py
+++ extract_collocations.py
@@ -1,26 +1,26 @@
-import os
+import pathlib
 
 import pandas as pd
 
-folder = "GF2.1-Collocations_JOS-structures"
-all_files = os.listdir(folder)
-all_files = [x for x in all_files if x.endswith(".csv")]
 
-all = []
-for f in all_files:
-    df = pd.read_csv(os.path.join(folder, f), encoding="utf8", sep=",")
-    for idx, row in df.iterrows():
-        w1 = str(row["C1_Lemma"])
-        w2 = str(row["C2_Lemma"])
-        w3 = str(row["C3_Lemma"])
-        if int(row["Frequency"]) > 100:
-            collocation = w1 + " " + w2
-            if w3 != "nan":
-                collocation = collocation + " " + w3
-                all.append((collocation, int(row["Frequency"])))
-    all = sorted(all, key=lambda x: x[1], reverse=True)[:600]
+NUM_COLLOCATIONS = 600
+FREQ_THRESHOLD = 100
+ROOT_DIR = pathlib.Path("GF2.1-Collocations_JOS-structures")
 
-with open("data/all_collocations.txt", "w", encoding="utf8") as o:
-    for collocation, freq in all:
-        print(collocation, freq)
-        o.write(collocation.strip() + "\n")
+subframes = []
+for fpath in ROOT_DIR.iterdir():
+    if not fpath.is_file() or not fpath.suffix == ".csv":
+        continue
+    subdf = pd.read_csv(fpath, encoding="utf8")
+    subdf = df[df["Frequency"] > FREQ_THRESHOLD]
+    subdf = df.dropna(subset="C3_Lemma")
+    subframes.append(subdf)
+
+df = pd.concat(subframes)
+df = df.sort_values(by=["Frequency"], ascending=False)
+df = all_df.iloc[:NUM_COLLOCATIONS]
+print(f"Top {NUM_COLOCATION} collocations:")
+print(df.to_csv(index=False))
+collocations = df[["C1_Lemma", "C2_Lemma", "C3_Lemma"]].apply(" ".join)
+collocations = collocation.apply(str.strip)
+collocation.to_csv("data/all_collocations.txt", header=False, index=False)
