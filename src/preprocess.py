1badbc62770bb8bad3b70ea14a014db202c94fbe
Marko Pranjic <marko.pranjic@ijs.si>
2022-04-28 13:11:25 +0200

    Do not use memory fs as communication channels in pandarallel
    We are probably hitting some limit and receive:
       EOFError: Ran out of input
    when shared memmory is used.

--- src/preprocess.py
+++ src/preprocess.py
@@ -18,7 +18,7 @@ from vocab import Vocab
 URL_RE = (
     r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
 )
-pandarallel.initialize(progress_bar=True)
+pandarallel.initialize(use_memory_fs=False, progress_bar=True)
 
 
 def get_stopwords(lang):
